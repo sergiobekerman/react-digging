beaa0b6d68df30b90d19d55a28b18f14e4eae810
Sergio Bekerman <sergio.bekerman@agilesight.com>
2019-05-14 17:44:52 -0300

    Created components and added to main.js so they are displayed on every page

--- my-demo/src/index.js
+++ my-demo/src/index.js
@@ -7,17 +7,39 @@ import Notfound from './NotFound'
 import Empty from './Empty'
 import * as serviceWorker from './serviceWorker';
 import { Route, Link, BrowserRouter, Switch } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import Topbar from './Topbar'
+import Footer from './Footer'
 
 
 const routing = (
     <BrowserRouter>
-      <div>
-      <Switch>
-        <Route exact path="/" component={Master} />
-        <Route path="/app" component={App} />
-        <Route path="/empty" component={Empty} />
-        <Route path component={Notfound} />
-      </Switch>
+      <div id="wrapper">
+      <Sidebar/>
+  
+  {/* <!-- Content Wrapper --> */}
+  <div id="content-wrapper" class="d-flex flex-column">
+
+    {/* <!-- Main Content --> */}
+    <div id="content">
+
+    <Topbar/>
+
+        <Switch>
+            <Route exact path="/" component={Master} />
+            <Route path="/app" component={App} />
+            <Route path="/empty" component={Empty} />
+            <Route path component={Notfound} />
+        </Switch>
+
+    </div>
+    {/* <!-- End of Main Content --> */}
+
+    <Footer/>
+  </div>
+  {/* <!-- End of Content Wrapper --> */}
+      
+
       </div>
     </BrowserRouter>
   )
